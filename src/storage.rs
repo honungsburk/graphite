f0b1cec3acec807f982c707bf069a510e42f7070
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2024-09-14 20:08:43 +0200

    flash out the events storage system

--- src/storage.rs
+++ src/storage.rs
@@ -1,7 +1,38 @@
+use crate::hlc;
+use crate::hlc::HLTimestamp;
 use uuid::Uuid;
+
+use serde::{Deserialize, Serialize};
 
 struct Storage {}
 
+impl Storage {
+    fn new() -> Storage {
+        Storage {}
+    }
+
+    fn play() {
+        // Play all events in order. Returns an iterator that must be consumed.
+        todo!("Implement play")
+    }
+
+    fn play_from(hlc: HLTimestamp) {
+        // Play all events in order starting from the given timestamp. Returns an iterator that must be consumed.
+        todo!("Implement play_from")
+    }
+
+    fn record(envelope: Event) {
+        // Record an event
+        todo!("Implement record")
+    }
+
+    fn record_batch(envelopes: Vec<Event>) {
+        // Record a batch of events
+        todo!("Implement record_batch")
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 enum DataTypes {
     String(String),
     Integer(i64),
@@ -11,7 +42,8 @@ enum DataTypes {
     Entity(Uuid),
 }
 
-enum Event {
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+enum Action {
     CreateEntity {
         id: Uuid,
     },
@@ -28,13 +60,39 @@ enum Event {
         id: Uuid,
     },
     Transaction {
-        events: Vec<Event>,
+        actions: Vec<Action>,
     },
 }
 
-struct Envelope {
-    hlc: u64,          // Logical timestamp
-    event_type: Event, // The type of event
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+struct Event {
+    id: Uuid,         // The unique identifier of the event
+    hlc: HLTimestamp, // Hybrid Logical timestamp
+    action: Action,   // The event that was performed
     actor: Uuid, // The actor who performed the event, e.g. a user. should be present amoung the entities
-    version: u64, // Envelope version
+    version: u32, // Event version
+}
+
+struct EventCreator {
+    actor: Uuid,
+    hlc: hlc::State<fn() -> i64>,
+}
+
+impl EventCreator {
+    fn new(actor: Uuid, hlt: HLTimestamp) -> EventCreator {
+        let mut hlc = hlc::State::new();
+        hlc.update(hlt); // Update the HLC with the given timestamp to have the correct time
+        EventCreator { actor, hlc }
+    }
+
+    fn create(&mut self, action: Action) -> Event {
+        let hlc = self.hlc.get_time();
+        Event {
+            id: Uuid::new_v4(),
+            hlc,
+            action,
+            actor: self.actor,
+            version: 0,
+        }
+    }
 }
